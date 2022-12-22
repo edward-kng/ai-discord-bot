6a8e4291f4e2181a76fbe38d06c957e6cefc856a
Edward Kang <edward.kang@protonmail.com>
2022-12-22 16:02:05 +0000

    Clear cache recursively in subdirectories

--- run.py
+++ run.py
@@ -11,15 +11,24 @@ def main():
     
     TOKEN = os.getenv('DISCORD_BOT_TOKEN')
 
-    clear_cache()
-    bot.run(TOKEN)
-
-def clear_cache():
     try:
-        for file in os.listdir("cache"):
-            os.remove("cache/" + file)
+        clear_cache("cache")
     except FileNotFoundError:
         os.mkdir("cache")
+    
+    bot.run(TOKEN)
+
+def clear_cache(path):
+    for file in os.listdir(path):
+        file_path = path + "/" + file
+
+        if os.path.isdir(file_path):
+            try:
+                os.rmdir(file_path)
+            except OSError:
+                clear_cache(file_path)
+        else:
+            os.remove(file_path)
 
 if __name__ == "__main__":
     main()
