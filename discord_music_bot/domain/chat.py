26a5204320cfe5312e1ed99232db01216dd91664
Edward Kang <edward.kang@protonmail.com>
2023-06-18 11:39:51 +0200

    Move create_completion into ChatService class

--- discord_music_bot/domain/chat.py
+++ discord_music_bot/domain/chat.py
@@ -1,30 +1,7 @@
 import asyncio
-
 import openai
 
 from .history import download_history
-
-
-def create_completion(chat_history, question, bot):
-    history_prompt = ""
-    chat_history["messages"].pop(0)
-
-    for message in reversed(chat_history["messages"]):
-        history_prompt += message["sender"]["name"] + " said: " + message["messageContent"] + "\n"
-
-    return openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {
-                "role": "system",
-                "content":
-                    "You are a Discord bot that chats with users. Your name is " + bot.user.name
-                    + ". Below is a transcript of "
-                    + "the conversation so far:\n" + history_prompt
-            },
-            {"role": "user", "content": question}
-        ]
-    )["choices"][0]["message"]["content"]
 
 
 class ChatService:
@@ -33,10 +10,34 @@ class ChatService:
         self.name = name
 
     async def answer(self, channel, question):
-
         if not openai.api_key:
             return "Chat not enabled!"
 
-        chat_history = await download_history(channel, limit=self.memory, download_images=False)
+        chat_history = await download_history(
+            channel, limit=self.memory, download_images=False
+        )
 
-        return await asyncio.to_thread(create_completion, chat_history, question, self.name)
+        return await asyncio.to_thread(
+            self.create_completion, chat_history, question
+        )
+
+    def create_completion(self, chat_history, question):
+        history_prompt = ""
+        chat_history["messages"].pop(0)
+
+        for message in reversed(chat_history["messages"]):
+            history_prompt += message["sender"]["name"] + " said: " + message["messageContent"] + "\n"
+
+        return openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {
+                    "role": "system",
+                    "content":
+                        "You are a Discord bot that chats with users. Your name is " + self.name.user.name
+                        + ". Below is a transcript of "
+                        + "the conversation so far:\n" + history_prompt
+                },
+                {"role": "user", "content": question}
+            ]
+        )["choices"][0]["message"]["content"]
